d737ae1dfa918bcd1896c069802840cf71f0896c
Johannes Blaschke <jpblaschke@lbl.gov>
2022-08-24 16:21:00 -0700

    Use 4-space indent

--- sh.lua
+++ sh.lua
@@ -2,112 +2,112 @@ local M = {}
 
 -- converts key and it's argument to "-k" or "-k=v" or just ""
 local function arg(k, a)
-	if not a then return k end
-	if type(a) == 'string' and #a > 0 then return k..'=\''..a..'\'' end
-	if type(a) == 'number' then return k..'='..tostring(a) end
-	if type(a) == 'boolean' and a == true then return k end
-	error('invalid argument type', type(a), a)
+    if not a then return k end
+    if type(a) == 'string' and #a > 0 then return k..'=\''..a..'\'' end
+    if type(a) == 'number' then return k..'='..tostring(a) end
+    if type(a) == 'boolean' and a == true then return k end
+    error('invalid argument type', type(a), a)
 end
 
 -- converts nested tables into a flat list of arguments and concatenated input
 local function flatten(t)
-	local result = {args = {}, input = ''}
+    local result = {args = {}, input = ''}
 
-	local function f(t)
-		local keys = {}
-		for k = 1, #t do
-			keys[k] = true
-			local v = t[k]
-			if type(v) == 'table' then
-				f(v)
-			else
-				table.insert(result.args, v)
-			end
-		end
-		for k, v in pairs(t) do
-			if k == '__input' then
-				result.input = result.input .. v
-			elseif not keys[k] and k:sub(1, 1) ~= '_' then
-				local key = '-'..k
-				if #k > 1 then key = '-' ..key end
-				table.insert(result.args, arg(key, v))
-			end
-		end
-	end
+    local function f(t)
+        local keys = {}
+        for k = 1, #t do
+            keys[k] = true
+            local v = t[k]
+            if type(v) == 'table' then
+                f(v)
+            else
+                table.insert(result.args, v)
+            end
+        end
+        for k, v in pairs(t) do
+            if k == '__input' then
+                result.input = result.input .. v
+            elseif not keys[k] and k:sub(1, 1) ~= '_' then
+                local key = '-'..k
+                if #k > 1 then key = '-' ..key end
+                table.insert(result.args, arg(key, v))
+            end
+        end
+    end
 
-	f(t)
-	return result
+    f(t)
+    return result
 end
 
 -- returns a function that executes the command with given args and returns its
 -- output, exit status etc
 local function command(cmd, ...)
-	local prearg = {...}
-	return function(...)
-		local args = flatten({...})
-		local s = cmd
-		for _, v in ipairs(prearg) do
-			s = s .. ' ' .. v
-		end
-		for k, v in pairs(args.args) do
-			s = s .. ' ' .. v
-		end
+    local prearg = {...}
+    return function(...)
+        local args = flatten({...})
+        local s = cmd
+        for _, v in ipairs(prearg) do
+            s = s .. ' ' .. v
+        end
+        for k, v in pairs(args.args) do
+            s = s .. ' ' .. v
+        end
 
-		if args.input then
-			local f = io.open(M.tmpfile, 'w')
-			f:write(args.input)
-			f:close()
-			s = s .. ' <'..M.tmpfile
-		end
-		local p = io.popen(s, 'r')
-		local output = p:read('*a')
-		local _, exit, status = p:close()
-		os.remove(M.tmpfile)
+        if args.input then
+            local f = io.open(M.tmpfile, 'w')
+            f:write(args.input)
+            f:close()
+            s = s .. ' <'..M.tmpfile
+        end
+        local p = io.popen(s, 'r')
+        local output = p:read('*a')
+        local _, exit, status = p:close()
+        os.remove(M.tmpfile)
 
-		local t = {
-			__input = output,
-			__exitcode = exit == 'exit' and status or 127,
-			__signal = exit == 'signal' and status or 0,
-		}
-		local mt = {
-			__index = function(self, k, ...)
-				return _G[k] --, ...
-			end,
-			__tostring = function(self)
-				-- return trimmed command output as a string
-				return self.__input:match('^%s*(.-)%s*$')
-			end
-		}
-		return setmetatable(t, mt)
-	end
+        local t = {
+            __input = output,
+            __exitcode = exit == 'exit' and status or 127,
+            __signal = exit == 'signal' and status or 0,
+        }
+        local mt = {
+            __index = function(self, k, ...)
+                return _G[k] --, ...
+            end,
+            __tostring = function(self)
+                -- return trimmed command output as a string
+                return self.__input:match('^%s*(.-)%s*$')
+            end
+        }
+        return setmetatable(t, mt)
+    end
 end
 
 -- get global metatable
 local mt = getmetatable(_G)
 if mt == nil then
-  mt = {}
-  setmetatable(_G, mt)
+    mt = {}
+    setmetatable(_G, mt)
 end
 
 local function list_contains(v, t)
-        for _, kv in pairs(v) do
-                if kv == t:sub(1, #kv)
-                then
-                        return true
-                end
+    for _, kv in pairs(v) do
+        if kv == t:sub(1, #kv)
+        then
+            return true
         end
-        return false
+    end
+    return false
 end
 
 __index_ignore = {"_G", "_PROMPT"}
 
 -- set hook for undefined variables
 mt.__index = function(t, cmd)
-        if list_contains(__index_ignore, cmd)
-        then
-                return rawget(t, cmd)
-        end
-	return command(cmd)
+    if list_contains(__index_ignore, cmd)
+    then
+        return rawget(t, cmd)
+    end
+    return command(cmd)
 end
 
 -- export command() function and configurable temporary "input" file
@@ -116,9 +116,9 @@ M.tmpfile = '/tmp/shluainput'
 
 -- allow to call sh to run shell commands
 setmetatable(M, {
-	__call = function(_, cmd, ...)
-		return command(cmd, ...)
-	end
+    __call = function(_, cmd, ...)
+        return command(cmd, ...)
+    end
 })
 
 return M
